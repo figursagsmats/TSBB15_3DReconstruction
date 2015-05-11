711ccd7b8ae1c0518a4baeee3b61c8b6814feb46
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-05-11 20:12:03 +0200

    Deleted leftovers

--- src/estimate_essential_matrix_test.m
+++
@@ -1,9 +0,0 @@
-pointsTable = load_dino_2dpts_gt();
-K = calibrate_camera('blabla', 'bavkbak');
-[corrPts1, corrPts2] = get_correspondces(1,2,pointsTable);
-
-E = estimate_essential_matrix(corrPts1, corrPts2, K);
-
-
-
-error = corrPts1'*E*corrPts2
