4767feaf550c0a39930d604a6538d3c41d4f17b2
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-05-11 13:10:35 +0200

    Added estimation for Rt and F. TODO: 1. Change name to estimate_fundamental_matrix. 2. Add RANSAC.

--- src/estimate_rt.m
+++ src/estimate_rt.m
@@ -1,6 +1,57 @@
-function [ Rt ] = estimate_rt( E,pt1,pt2 )
+function [ Rt ] = estimate_rt( E,corr_point1,corr_point2 )
+% CALCULATE_Rt - A camera pose Rt consistent with E
+% 
+% Calculate a relative camera pose Rt that is consistent
+% with the essential matrix E and for which the 3D-point 
+% that corresponds to corr_point1 and corr_point2 lies in front
+% of both cameras.
+% --------------------------------------------
+% Input: An essential matrix E.
+%        A pair of correspondence points corr_point1 and corr_point2.
+%        with dimension: 2x1
+% Output: Rotation and translation matrix Rt.
+% 
 
-%FORNOW:
-Rt = eye(3,4); 
+W = [0 1 0;
+    -1 0 0;
+     0 0 1];
+
+[U, ~, V] = svd(E);
+tPositiveDir = V(:,3);
+tNegativeDir = -V(:,3);
+
+t(:,1) = tPositiveDir;
+t(:,2) = tNegativeDir;
+
+R(:,:,1) = V*W*U';
+R(:,:,2) = V*W'*U';
+
+Rt1 = [1 0 0 0;
+       0 1 0 0;
+       0 0 1 0];
+
+  
+for tDirection = 1:2
+    for RDirection = 1:2
+        
+        % Pick an Rt.
+        Rt2 = [R(:,:,RDirection) t(:,tDirection)];
+        % Get 3D-point for Rt1 and Rt2, where Rt1 is unit
+        X_homogenous = triangulate_optimal(Rt1, Rt2, corr_point1, corr_point2);
+        X_homogenous = norml(X_homogenous);
+        X = X_homogenous(1:3);
+        X2 = R(:,:,RDirection)*X + t(:,tDirection);
+        
+        z = X2(3);
+        z2 = X(3);
+        
+        % On one of the four cases, we get a point
+        % infront of our cameras. Save the Rt for that case.
+        if (z > 0) && (z2 > 0)
+            Rt = Rt2;
+        end
+    end
+end
+
 end
 
