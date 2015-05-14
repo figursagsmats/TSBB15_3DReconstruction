bc3b80806feed3a76eaf10255e3cbd161589eb1a
KristoferJanukiewicz <kristofer.janukiewicz@gmail.com>
2015-05-14 11:40:13 +0200

    *Removed a comment I added yesterday in tsbb15_3dreconstruction.m *Restructure of the functionality in bundle_adjustment.m (still under progress) *Changed some lines in dpp.m, this is due to the restructure of bundle_adjustment.m (still under progress) *Deleted is_visibility.m and added visibility_function.m. The visibility_function.m still does the same thing as is_visibility.m. Added documentation to this function.

--- src/mainfunc/kri/dpp.m
+++ src/mainfunc/kri/dpp.m
@@ -6,15 +6,16 @@ function r = dpp(vars, N_VIEWS, Xt)
     Np = size(Xt,2);
     X = reshape(vars(((1+N_VIEWS)*12):end),[3 Np]);
      
-    r = 0;
+    % r = zeros(2,Np);
     for m = 1:N_VIEWS
-       Rt = reshape(vars(1+((m-1)*12):12+((m-1)*12)),[3 4]);
-       Xtk = Xt(:,:,m);
-       Wk = is_visibility(Xtk);
-       Xtk = Xtk.*Wk;
-       Xtp = Rt*[X;ones(1,Np)];
+        Rt = reshape(vars(1+((m-1)*12):12+((m-1)*12)),[3 4]);
+        Xtk = Xt(:,:,m);
+        Wk = is_visibility(Xtk);
+        Xtk = Xtk.*Wk;
+        Xtp = Rt*[X;ones(1,Np)];
 
-       r = r + sqrt((Xtk(1,:)-Xtp(1,:)./Xtp(3,:)).^2+(Xtk(2,:)-Xtp(2,:)./Xtp(3,:)).^2);
+        r = r + sqrt((Xtk(1,:)-Xtp(1,:)./Xtp(3,:)).^2+(Xtk(2,:)-Xtp(2,:)./Xtp(3,:)).^2);
+        % r(2,:)=sqrt((Xi(1,:)-Xip(1,:)./Xip(3,:)).^2+(Xi(2,:)-Xip(2,:)./Xip(3,:)).^2);
     end
 
 end
