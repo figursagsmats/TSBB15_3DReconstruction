5abe7fb3be6d0ab70ede8d5e07abf886b86e500c
KristoferJanukiewicz <kristofer.janukiewicz@gmail.com>
2015-05-18 04:24:32 +0200

    Added functionality to removeBad3DPoints.m. The function is not yet tested.

--- src/mainfuncionality/krija/removeBad3DPoints.m
+++ src/mainfuncionality/krija/removeBad3DPoints.m
@@ -7,11 +7,17 @@
 
 function [P,BK] = removeBad3DPoints(BK,Q,oldP,newP)
 
+
+    %P = newP;
+    
+    % Choose a threshold for X movement in distance
+    threshold_movement = 5.0;
+    
+    % Choose a threshold for reprojection errors
+    threshold_r = 0.5;
+    
     %% STEP 1 - Remove 3D-points that has made a large change in position
     
-    % Choose a threshold
-    threshold = 0.5;
-
     % Calculate the distance difference in every dimension
     distance_diff_P = zeros(size(oldP));
     distance_diff_P(1,:) = (oldP(1,:) - newP(1,:)).^2;
@@ -23,7 +29,7 @@ function [P,BK] = removeBad3DPoints(BK,Q,oldP,newP)
     distance_diff_XYZ = sqrt(distance_diff_P(1,:) + distance_diff_P(2,:) + distance_diff_P(3,:));
     
     % Find the indices of the large distance difference
-    large_indices = find(distance_diff_XYZ > threshold);
+    large_indices = find(distance_diff_XYZ > threshold_movement);
     
     % Remove points from P
     temp_P = newP;
@@ -33,8 +39,40 @@ function [P,BK] = removeBad3DPoints(BK,Q,oldP,newP)
     % Remove columns from BK
     BK(:,large_indices,:) = [];
     
-    %% STEP 2 - Remove 3D-points if their reprojection error is large 
+    %% STEP 2 - Make BK y_kj to -1 if their reprojection error is large 
     
+    % Make 3D-points to homogenous
+    X_new = conv_to_homogeneous(newP);
+    
+    % Create a visibility function W from BK
+    W = double(BK(:,:,1) > 0);
+    
+    % Get number of views
+    N_VIEWS = size(Q,2); 
+    
+    % Calculate the reprojection error in every point in every view
+    r = zeros(size(BK));
+    for m=1:N_VIEWS
+        C = Q(m).P;
+        r(m,:,1) = W(m,:).*(BK(m,:,1) - ( (C(1,:)*X_new) ./ (C(3,:)*X_new) ));
+        r(m,:,2) = W(m,:).*(BK(m,:,2) - ( (C(2,:)*X_new) ./ (C(3,:)*X_new) ));
+    end
+    
+    % Calculate reprojection euclidian distance
+    r_distance = sqrt(r(:,:,1).^2 + r(:,:,2).^2);
+    r_thresholded = double(r_distance < threshold_r);
+    BK(:,:,1) = BK(:,:,1).*r_thresholded;
+    BK(:,:,2) = BK(:,:,2).*r_thresholded;
+    BK(BK == 0) = -1;
+    
+    r_thresholded_sum = sum(r_thresholded,1);
+    outlier_removal_r_index = find(r_thresholded_sum > (N_VIEWS-1));
+    
+    % Remove columns from P
+    P(:,outlier_removal_r_index) = [];
+    
+    % Remove columns from BK
+    BK(:,outlier_removal_r_index,:) = [];
     
 end
 
